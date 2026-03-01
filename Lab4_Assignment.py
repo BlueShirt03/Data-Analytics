da70273100f8af514be6abc7eeb4d72962139f56
BlueShirt03 <157056676+BlueShirt03@users.noreply.github.com>
2026-02-28 21:37:47 -0600

    Version 40

--- Lab4_Assignment.py
+++ Lab4_Assignment.py
@@ -11,15 +11,11 @@ df_large = pd.DataFrame(data)
 
 df_dask = dd.from_pandas(df_large, npartitions=10)
 
-
 simple_imputer = SimpleImputer(strategy='mean')
-
 
 df_dask_imputed = df_dask.map_partitions(lambda df: pd.DataFrame(simple_imputer.fit_transform(df), columns=df.columns))
 
-
 df_dask_imputed = df_dask_imputed.compute()
-
 
 print(df_dask_imputed.head())
 
