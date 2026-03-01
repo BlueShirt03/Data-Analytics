e2e24bb4f0f3823b7eb6978379b0ba209e9d0e78
BlueShirt03 <157056676+BlueShirt03@users.noreply.github.com>
2026-02-28 21:34:44 -0600

    Version 39: took out some stuff

--- Lab4_Assignment.py
+++ Lab4_Assignment.py
@@ -9,18 +9,17 @@ data = {'Age': [25, None, 22, 35, None] * 200000,
 
 df_large = pd.DataFrame(data)
 
-# Convert the Pandas dataframe to a Dask dataframe
 df_dask = dd.from_pandas(df_large, npartitions=10)
 
-# Define a SimpleImputer
+
 simple_imputer = SimpleImputer(strategy='mean')
 
-# Apply the imputer on the Dask dataframe
+
 df_dask_imputed = df_dask.map_partitions(lambda df: pd.DataFrame(simple_imputer.fit_transform(df), columns=df.columns))
 
-# Compute the result
+
 df_dask_imputed = df_dask_imputed.compute()
 
-# View the first few rows of the imputed dataframe
+
 print(df_dask_imputed.head())
 
