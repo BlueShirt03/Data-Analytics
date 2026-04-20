3b57bc58e1d83e400410037a1776cc77928f616d
BlueShirt03 <157056676+BlueShirt03@users.noreply.github.com>
2026-04-20 14:29:18 -0500

    This lab removes values that are out of range for a creatin range that is set

---
+++ Lab8_Assignment.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+
+data = {'Name': ['Alice', 'Bob', 'Charlie', 'Diana'],
+        'Age': [25, 132, 30, -5]}
+df = pd.DataFrame(data)
+
+out_of_range = df[(df['Age'] < 0) | (df['Age'] > 120)]
+print(out_of_range)
+
+df = df[(df['Age'] >= 0) & (df['Age'] <= 120)]
+
+
+print("\\nDataset after removing out-of-range values:")
+print(df)
